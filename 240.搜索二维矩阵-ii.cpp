bdee9d755417f9484b4ea2957b32ca3c95c4b7e9
Jiamin Liu <carminljm@163.com>
2021-07-11 21:03:58 +0800

    commit

--- 240.搜索二维矩阵-ii.cpp
+++ 240.搜索二维矩阵-ii.cpp
@@ -10,8 +10,14 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
         if(m==0 || n==0 ) return false;
-
-        int lef
+        int row = m-1, col = 0;
+        while(row>=0 && col <n)
+        {
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col]>target) row--;
+            else col++;
+        }
+        return false;
     }
 };
 // @lc code=end
